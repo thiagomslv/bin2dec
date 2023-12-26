0f9fbecbf02eeeb964765f39b46cdb325f1e521e
Thiago Marques <thiagomarquessilva99@gmail.com>
2023-12-26 11:54:47 -0300

    Preparação do layout para receber a lógica

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const [result, setResult] = useState(0)
+
   return (
-    <div>
+    <div className='conatiner'>
+
+      <h1>Conversor binário para decimal</h1>
+      <div className='display'><span className='result'>{result}</span></div>
+
+      <input type="text" className='binary-text'/>
 
     </div>
   )
